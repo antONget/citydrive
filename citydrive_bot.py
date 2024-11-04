cb6ef9034559813316afb72f96abe3ba33737818
antONget <a.l.ponomare@mail.ru>
2024-11-04 10:26:17 +0300

    realize 2

--- citydrive_bot.py
+++ citydrive_bot.py
@@ -4,6 +4,8 @@ from aiogram import Bot, Dispatcher
 from config_data.config import Config, load_config
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from handlers.shceduler import parsing_google_sheets_day, parsing_google_sheets_night
+from aiogram.types import ErrorEvent, FSInputFile
+import traceback
 # Инициализируем logger
 logger = logging.getLogger(__name__)
 
@@ -31,10 +33,21 @@ async def main():
     scheduler = AsyncIOScheduler(timezone="Europe/Moscow")
 
     # каждый день
-    scheduler.add_job(parsing_google_sheets_day, 'cron', hour=9, minute=50, args=(bot,))
+    scheduler.add_job(parsing_google_sheets_day, 'cron', hour=10, minute=27, args=(bot,))
     scheduler.add_job(parsing_google_sheets_night, 'cron', hour=21, minute=50, args=(bot,))
     scheduler.start()
 
+    @dp.error()
+    async def error_handler(event: ErrorEvent):
+        logger.critical("Критическая ошибка: %s", event.exception, exc_info=True)
+        await bot.send_message(chat_id=843554518,
+                               text=f'{event.exception}')
+        formatted_lines = traceback.format_exc()
+        text_file = open('error.txt', 'w')
+        text_file.write(str(formatted_lines))
+        text_file.close()
+        await bot.send_document(chat_id=843554518,
+                                document=FSInputFile('error.txt'))
     # Пропускаем накопившиеся update и запускаем polling
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
